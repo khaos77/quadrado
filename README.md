b2ea57c71b9121817f75c2d3c65db8146b1e31f0
khaos77 <contato.isaa0l@gmail.com>
2023-10-03 21:59:23 -0300

    primeiro pra dar sorte

---
+++ README.md
@@ -0,0 +1,7 @@
+## Small project with a square.
+
+The hole challenge was to make a square perimeter's made by "a", with python language using for loop statement.
+
+In the near future, I am willing to evolve this project, practicing with different geometric forms and using other ways to format it.
+
+If there is any tip or any advice feel free to coment!
---
+++ square.py
@@ -0,0 +1,23 @@
+altura = int(input("meidda da altura: "))
+largura = int(input("medida da largura: "))
+ab = "a"
+
+if altura == 1:
+    print(ab * (largura))
+
+elif largura == 1:
+    for c in range(altura):
+        print(ab)
+elif altura < 0 or largura <0:
+    print()
+
+else:
+    print(ab * largura)
+    for b in range(altura - 2):
+        print(ab + " " *(largura -2)+ ab)
+        '''
+        printa o primeiro, espaços em branco -2(correspondendo ao 
+        primeiro e o útltimo) e o último a
+        '''
+
+    print(ab * largura)
